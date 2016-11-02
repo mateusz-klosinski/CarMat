972bfaecf1aeca6cdc00710cf296f7e875d91a01
Mateusz Klosinski <matix003@gmail.com>
2016-11-02 01:24:57 +0100

    Layout, responsive images, responsive animations

--- CarMat/src/CarMat/wwwroot/js/site.js
+++ CarMat/src/CarMat/wwwroot/js/site.js
@@ -1,1 +1,28 @@
 ﻿// Write your Javascript code.
+
+(function () {
+
+    $(window).on("load resize", function () {
+
+        var images = $(".top-vehicles li img");
+
+        images.each(function () {
+            $(this).css("height", "auto");
+        });
+
+        if (window.innerWidth > 1200) {
+            var height = 0;
+
+            images.each(function () {
+                if ($(this).height() > height) {
+                    height = $(this).height();
+                }
+            });
+
+            images.each(function () {
+                $(this).css("height", height);
+            });
+        }
+    });
+}());
+
